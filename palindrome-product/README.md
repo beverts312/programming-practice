a75fd026533a5cc984464a0cec8f96a13646664d
beverts312 <texas312@gmail.com>
2016-12-06 00:03:55 -0600

    add project euler question 4

---
+++ palindrome-product/README.md
@@ -0,0 +1,11 @@
+# Palindrome Product  
+Source: [Project Euler](https://projecteuler.net/problem=4)  
+Description: Find the largest palindrome made from the product of two 3-digit numbers.  
+
+## Typescript  
+Check it out [here](./node/src/main.ts).  
+To run (assumes you arleady have node/npm):  
+1. Navigate to the `node` directory  
+2. Install dev dependencies `npm install`  
+3. Transpile: `npm run build`  
+4. Execute: `npm run start`
---
+++ palindrome-product/node/src/main.ts
@@ -0,0 +1,31 @@
+// Find the largest palindrome made from the product of two 3-digit numbers.
+
+/**
+ * Checks if the input is a palindrome
+ * @param {number} x
+ * @returns {boolean}
+ */
+const isPalindrome = (x: number): boolean => {
+    let xStr = x.toString();
+    let length = xStr.length;
+    for (let i = 0; i < length; i++) {
+        if (xStr.charAt(i) !== xStr.charAt((length - 1) - i)) {
+            return false;
+        }
+    }
+    return true;
+};
+
+let t = 0;
+let possible = [];
+for (let i = 999; i > 100; i--) {
+    for (let j = 999; j > 100; j--) {
+        t = i * j;
+        if (isPalindrome(t)) {
+            possible.push(t);
+        }
+    }
+}
+possible.sort((a, b) => { return b - a; });
+console.log(possible[0]);
+// 906609
