afc021d58dc71f6a4b58663fd687a800b16308fc
beverts312 <texas312@gmail.com>
2016-12-05 21:35:15 -0600

    add question 2 from project euler

---
+++ sum-of-fib-below-4-million/READEME.md
@@ -0,0 +1,11 @@
+# Even Fibonacci numbers  
+Source: [Project Euler](https://projecteuler.net/problem=2)  
+Description: By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.  
+
+## Typescript  
+Check it out [here](./node/src/main.ts).  
+To run (assumes you arleady have node/npm):  
+1. Navigate to the `node` directory  
+2. Install dev dependencies `npm install`  
+3. Transpile: `npm run build`  
+4. Execute: `npm run start`
---
+++ sum-of-fib-below-4-million/node/src/main.ts
@@ -0,0 +1,25 @@
+//By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.  
+const fibonacci = (x: number) => {
+    if (x <= 1) {
+        return 1;
+    }
+    return fibonacci(x - 1) + fibonacci(x - 2);
+}
+
+const isEven = (x: number) => {
+    return x % 2 ? false : true;
+}
+
+const max = 4000000;
+let answer = 0
+let n = 0;
+let i = 2;
+
+while (n < max) {
+    if (isEven(n) == true) {
+        answer += n;
+    }
+    n = fibonacci(i);
+    i++;
+}
+console.log(answer);
