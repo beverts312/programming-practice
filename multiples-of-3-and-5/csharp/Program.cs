a81f111e2fd6537adf9ee72b968983927b34c207
beverts312 <texas312@gmail.com>
2016-12-07 15:37:33 -0600

    added csharp solution to problem 1

---
+++ multiples-of-3-and-5/csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+using System;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static bool isMultipleOfThreeOrFive(int x) 
+        {
+            return (x % 3 == 0 ? true : false) || (x % 5 == 0 ? true : false);
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Enumerable.Range(0, 1000).Where(x => isMultipleOfThreeOrFive(x) == true).Sum());
+        }
+    }
+}
+// 233168
